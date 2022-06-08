a783113c2f07cb0f1fdaa11582bc6c459448d18a
Daniel Sam <daniellevilucas@gmail.com>
2022-06-08 15:09:55 +0530

    Import taggit and add it to Post schema

--- blog/models.py
+++ blog/models.py
@@ -2,16 +2,13 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 # Create your models here.
 
 
 class Post(models.Model):
-    STATUS_CHOICES = (
-        ('draft', 'Draft'),
-        ('published', 'Published'),
-    )
-
+    tags = TaggableManager()
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250,
                             unique_for_date='publish')
@@ -24,6 +21,11 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
