29f59c24fa83a68d8cb4e90f7c0550a291f575ea
Daniel Sam <daniellevilucas@gmail.com>
2022-06-09 11:39:39 +0530

    Add feed route to urlpattterns

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .feeds import LatestPostsFeed
 from . import views
 
 app_name = 'blog'
@@ -11,4 +12,5 @@ urlpatterns = [
          views.post_share, name='post_share'),
     path('tag/<slug:tag_slug>/',
          views.post_list, name='post_list_by_tag'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
