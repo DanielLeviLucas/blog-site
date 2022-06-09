d311ddc1c12a6c98a6f6bfd4bd7499259025bdde
Daniel Sam <daniellevilucas@gmail.com>
2022-06-09 13:19:58 +0530

    Use stemming in post_search
    Stemming is the process of reducing words
    to their word stem, base, or root form.
    Stemming is used by search engines to reduce indexed words
    to their stem, and to be able to match inflected or derived words.
    For example, "music" and "musician"
    can be considered similar words by a search engine.

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from taggit.models import Tag
 from django.db.models import Count
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm, SearchForm
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 # Create your views here.
@@ -99,9 +99,13 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.objects.filter(status='published').annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
+
     return render(request, 'blog/post/search.html',
                   {'form': form,
                    'query': query,
